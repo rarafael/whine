355d04ff244be1c0e208b47f3358b5c9db709c69
rexim <reximkut@gmail.com>
2021-11-26 00:15:42 +0700

    Add sliders for volume

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 set -xe
 
-CFLAGS="-Wall -Werror -std=c11 -pedantic -ggdb `pkg-config --cflags sdl2`"
+CFLAGS="-Wall -Wextra -Wshadow -Werror -std=c11 -pedantic -ggdb `pkg-config --cflags sdl2`"
 LIBS="`pkg-config --libs sdl2` -lm"
 
 cc $CFLAGS -o whine main.c $LIBS
--- main.c
+++ main.c
@@ -8,10 +8,7 @@
 
 #define FREQ 48000
 #define SAMPLE_DT (1.0f / FREQ)
-#define STEP_TIME_MIN 1.0f
-#define STEP_TIME_MAX 200.0f
 
-// TODOOO: Add sliders for volume
 // TODOO: Visualize a smol section of the white noise sound wave
 // TODO: Mix in more randomness into the generated white noise
 
@@ -19,6 +16,7 @@ typedef struct {
     Sint16 current;
     Sint16 next;
     float step_time;
+    float volume;
     float a;
 } Gen;
 
@@ -45,7 +43,7 @@ void white_noise(Gen *gen, Sint16 *stream, size_t stream_len)
 
     for (size_t i = 0; i < stream_len; ++i) {
         gen->a += gen_step * SAMPLE_DT;
-        stream[i] = lerpf(gen->next, gen->current, cosf(gen->a));
+        stream[i] = lerpf(gen->next, gen->current, cosf(gen->a)) * gen->volume;
 
         if (gen->a >= 1.0f) {
             Sint16 value = rand() % (1 << 10);
@@ -71,6 +69,74 @@ void white_noise_callback(void *userdata, Uint8 *stream, int len)
 
 #define BACKGROUND_COLOR 0x181818FF
 
+#define STEP_TIME_SLIDER_THICC 5.0f
+#define STEP_TIME_SLIDER_COLOR 0x00FF00FF
+#define STEP_TIME_GRIP_SIZE 10.0f
+#define STEP_TIME_GRIP_COLOR 0xFF0000FF
+
+int hot_id = -1;
+int active_id = -1;
+
+void slider(SDL_Renderer *renderer, int id,
+            float pos_x, float pos_y, float len,
+            float *value, float min, float max)
+{
+    // Slider Body
+    {
+        SDL_SetRenderDrawColor(renderer, HEXCOLOR(STEP_TIME_SLIDER_COLOR));
+        SDL_Rect rect = {
+            .x = pos_x,
+            .y = pos_y - STEP_TIME_SLIDER_THICC * 0.5f,
+            .w = len,
+            .h = STEP_TIME_SLIDER_THICC,
+        };
+        SDL_RenderFillRect(renderer, &rect);
+    }
+
+    // Grip
+    {
+        assert(min <= max);
+        float grip_value = ilerpf(min, max, *value) * len;
+
+        SDL_SetRenderDrawColor(renderer, HEXCOLOR(STEP_TIME_GRIP_COLOR));
+        SDL_Rect rect = {
+            .x = pos_x - STEP_TIME_GRIP_SIZE + grip_value,
+            .y = pos_y - STEP_TIME_GRIP_SIZE,
+            .w = STEP_TIME_GRIP_SIZE * 2.0f,
+            .h = STEP_TIME_GRIP_SIZE * 2.0f,
+        };
+        SDL_RenderFillRect(renderer, &rect);
+
+        int x, y;
+        Uint32 buttons = SDL_GetMouseState(&x, &y);
+
+        if (active_id < 0) {
+            SDL_Point cursor = {x, y};
+            if (SDL_PointInRect(&cursor, &rect) && (buttons & SDL_BUTTON_LMASK) != 0) {
+                active_id = id;
+            }
+        } else {
+            if (active_id == id) {
+                if ((buttons & SDL_BUTTON_LMASK) == 0) {
+                    active_id = -1;
+                } else {
+                    float grip_min = pos_x - STEP_TIME_GRIP_SIZE;
+                    float grip_max = grip_min + len;
+                    float xf = clampf(x - STEP_TIME_GRIP_SIZE, grip_min, grip_max);
+                    xf = ilerpf(grip_min, grip_max, xf);
+                    xf = lerpf(min, max, xf);
+                    *value = xf;
+                }
+            }
+        }
+    }
+}
+
+typedef enum {
+    SLIDER_FREQ = 0,
+    SLIDER_VOLUME,
+} Slider;
+
 int main(void)
 {
     if (SDL_Init(SDL_INIT_AUDIO | SDL_INIT_VIDEO) < 0) {
@@ -92,6 +158,7 @@ int main(void)
 
     Gen gen = {
         .step_time = 50.0f,
+        .volume = 1.0f,
     };
 
     SDL_AudioSpec desired = {
@@ -110,7 +177,6 @@ int main(void)
     SDL_PauseAudio(0);
 
     bool quit = false;
-    bool dragging_grip = false;
     while (!quit) {
         SDL_Event event;
         while (SDL_PollEvent(&event)) {
@@ -125,60 +191,12 @@ int main(void)
         SDL_SetRenderDrawColor(renderer, HEXCOLOR(BACKGROUND_COLOR));
         SDL_RenderClear(renderer);
 
-#define STEP_TIME_SLIDER_X 100.0f
-#define STEP_TIME_SLIDER_Y 100.0f
-#define STEP_TIME_SLIDER_LEN 500.0f
-#define STEP_TIME_SLIDER_THICC 5.0f
-#define STEP_TIME_SLIDER_COLOR 0x00FF00FF
-#define STEP_TIME_GRIP_SIZE 10.0f
-#define STEP_TIME_GRIP_COLOR 0xFF0000FF
-
-        // Slider Body
-        {
-            SDL_SetRenderDrawColor(renderer, HEXCOLOR(STEP_TIME_SLIDER_COLOR));
-            SDL_Rect rect = {
-                .x = STEP_TIME_SLIDER_X,
-                .y = STEP_TIME_SLIDER_Y - STEP_TIME_SLIDER_THICC * 0.5f,
-                .w = STEP_TIME_SLIDER_LEN,
-                .h = STEP_TIME_SLIDER_THICC,
-            };
-            SDL_RenderFillRect(renderer, &rect);
-        }
-
-        // Grip
-        {
-            float grip_value = gen.step_time / (STEP_TIME_MAX - STEP_TIME_MIN) * STEP_TIME_SLIDER_LEN;
-
-            SDL_SetRenderDrawColor(renderer, HEXCOLOR(STEP_TIME_GRIP_COLOR));
-            SDL_Rect rect = {
-                .x = STEP_TIME_SLIDER_X - STEP_TIME_GRIP_SIZE + grip_value,
-                .y = STEP_TIME_SLIDER_Y - STEP_TIME_GRIP_SIZE,
-                .w = STEP_TIME_GRIP_SIZE * 2.0f,
-                .h = STEP_TIME_GRIP_SIZE * 2.0f,
-            };
-            SDL_RenderFillRect(renderer, &rect);
-
-            int x, y;
-            Uint32 buttons = SDL_GetMouseState(&x, &y);
-
-            if (!dragging_grip) {
-                SDL_Point cursor = {x, y};
-                if (SDL_PointInRect(&cursor, &rect) && (buttons & SDL_BUTTON_LMASK) != 0) {
-                    dragging_grip = true;
-                }
-            } else {
-                if ((buttons & SDL_BUTTON_LMASK) == 0) {
-                    dragging_grip = false;
-                } else {
-                    float grip_min = STEP_TIME_SLIDER_X - STEP_TIME_GRIP_SIZE;
-                    float grip_max = grip_min + STEP_TIME_SLIDER_LEN;
-                    float xf = clampf(x - STEP_TIME_GRIP_SIZE, grip_min, grip_max);
-                    xf = ilerpf(grip_min, grip_max, xf);
-                    xf = lerpf(STEP_TIME_MIN, STEP_TIME_MAX, xf);
-                    gen.step_time = xf;
-                }
-            }
-        }
+        slider(renderer, SLIDER_FREQ,
+               100.0f, 100.0f, 500.0f,
+               &gen.step_time, 1.0f, 200.0f);
+        slider(renderer, SLIDER_VOLUME,
+               100.0f, 200.0f, 500.0f,
+               &gen.volume, 0.0f, 1.0f);
 
         SDL_RenderPresent(renderer);
     }
