fd1732a96df5bdd7e5ec84600749216a7873bf6d
rexim <reximkut@gmail.com>
2021-11-26 01:22:29 +0700

    Visualize a smol section of the white noise sound wave

--- main.c
+++ main.c
@@ -9,10 +9,10 @@
 #define FREQ 48000
 #define SAMPLE_DT (1.0f / FREQ)
 
-// TODOO: Visualize a smol section of the white noise sound wave
 // TODO: Mix in more randomness into the generated white noise
 
 typedef struct {
+    unsigned int seedp;
     Sint16 current;
     Sint16 next;
     float step_time;
@@ -43,11 +43,11 @@ void white_noise(Gen *gen, Sint16 *stream, size_t stream_len)
 
     for (size_t i = 0; i < stream_len; ++i) {
         gen->a += gen_step * SAMPLE_DT;
-        stream[i] = lerpf(gen->next, gen->current, cosf(gen->a)) * gen->volume;
+        stream[i] = lerpf(gen->next, gen->current, gen->a * gen->a) * gen->volume;
 
         if (gen->a >= 1.0f) {
-            Sint16 value = rand() % (1 << 10);
-            Sint16 sign = (rand() % 2) * 2 - 1;
+            Sint16 value = rand_r(&gen->seedp) % (1 << 10);
+            Sint16 sign = (rand_r(&gen->seedp) % 2) * 2 - 1;
             gen->current = gen->next;
             gen->next = value * sign;
             gen->a = 0.0f;
@@ -69,26 +69,54 @@ void white_noise_callback(void *userdata, Uint8 *stream, int len)
 
 #define BACKGROUND_COLOR 0x181818FF
 
-#define STEP_TIME_SLIDER_THICC 5.0f
-#define STEP_TIME_SLIDER_COLOR 0x00FF00FF
-#define STEP_TIME_GRIP_SIZE 10.0f
-#define STEP_TIME_GRIP_COLOR 0xFF0000FF
-
-int hot_id = -1;
 int active_id = -1;
 
-void slider(SDL_Renderer *renderer, int id,
+#define WAVE_PREVIEW_WIDTH 1000.0f
+#define WAVE_PREVIEW_HEIGHT 300.0f
+#define WAVE_PREVIEW_SAMPLE_COLOR 0xFFFF00FF
+
+void wave_preview(SDL_Renderer *renderer,
+                  float pos_x, float pos_y,
+                  Sint16 *stream, size_t stream_len)
+{
+    float sample_width = WAVE_PREVIEW_WIDTH / (float) stream_len;
+    float y_axis = pos_y + WAVE_PREVIEW_HEIGHT * 0.5f;
+
+    for (size_t i = 0; i < stream_len; ++i) {
+        float sample_x = pos_x + (float) i * sample_width;
+        float sample_y = lerpf(pos_y,
+                               pos_y + WAVE_PREVIEW_HEIGHT,
+                               ilerpf(-((1 << 10) - 1), ((1 << 10) - 1), stream[i]));
+
+        SDL_SetRenderDrawColor(renderer, HEXCOLOR(WAVE_PREVIEW_SAMPLE_COLOR));
+        SDL_Rect rect = {
+            .x = sample_x,
+            .y = y_axis,
+            .w = sample_width,
+            .h = sample_y - y_axis,
+        };
+        SDL_RenderFillRect(renderer, &rect);
+    }
+
+}
+
+#define SLIDER_THICCNESS 5.0f
+#define SLIDER_COLOR 0x00FF00FF
+#define SLIDER_GRIP_SIZE 10.0f
+#define SLIDER_GRIP_COLOR 0xFF0000FF
+
+bool slider(SDL_Renderer *renderer, int id,
             float pos_x, float pos_y, float len,
             float *value, float min, float max)
 {
     // Slider Body
     {
-        SDL_SetRenderDrawColor(renderer, HEXCOLOR(STEP_TIME_SLIDER_COLOR));
+        SDL_SetRenderDrawColor(renderer, HEXCOLOR(SLIDER_COLOR));
         SDL_Rect rect = {
             .x = pos_x,
-            .y = pos_y - STEP_TIME_SLIDER_THICC * 0.5f,
+            .y = pos_y - SLIDER_THICCNESS * 0.5f,
             .w = len,
-            .h = STEP_TIME_SLIDER_THICC,
+            .h = SLIDER_THICCNESS,
         };
         SDL_RenderFillRect(renderer, &rect);
     }
@@ -98,12 +126,12 @@ void slider(SDL_Renderer *renderer, int id,
         assert(min <= max);
         float grip_value = ilerpf(min, max, *value) * len;
 
-        SDL_SetRenderDrawColor(renderer, HEXCOLOR(STEP_TIME_GRIP_COLOR));
+        SDL_SetRenderDrawColor(renderer, HEXCOLOR(SLIDER_GRIP_COLOR));
         SDL_Rect rect = {
-            .x = pos_x - STEP_TIME_GRIP_SIZE + grip_value,
-            .y = pos_y - STEP_TIME_GRIP_SIZE,
-            .w = STEP_TIME_GRIP_SIZE * 2.0f,
-            .h = STEP_TIME_GRIP_SIZE * 2.0f,
+            .x = pos_x - SLIDER_GRIP_SIZE + grip_value,
+            .y = pos_y - SLIDER_GRIP_SIZE,
+            .w = SLIDER_GRIP_SIZE * 2.0f,
+            .h = SLIDER_GRIP_SIZE * 2.0f,
         };
         SDL_RenderFillRect(renderer, &rect);
 
@@ -120,22 +148,39 @@ void slider(SDL_Renderer *renderer, int id,
                 if ((buttons & SDL_BUTTON_LMASK) == 0) {
                     active_id = -1;
                 } else {
-                    float grip_min = pos_x - STEP_TIME_GRIP_SIZE;
+                    float grip_min = pos_x - SLIDER_GRIP_SIZE;
                     float grip_max = grip_min + len;
-                    float xf = clampf(x - STEP_TIME_GRIP_SIZE, grip_min, grip_max);
+                    float xf = clampf(x - SLIDER_GRIP_SIZE, grip_min, grip_max);
                     xf = ilerpf(grip_min, grip_max, xf);
                     xf = lerpf(min, max, xf);
                     *value = xf;
+                    return true;
                 }
             }
         }
     }
+
+    return false;
 }
 
 typedef enum {
+    // TODO: add a slider for the amount of preview samples
     SLIDER_FREQ = 0,
     SLIDER_VOLUME,
 } Slider;
+
+#define PREVIEW_STREAM_CAP 100
+Sint16 preview_stream[PREVIEW_STREAM_CAP];
+
+void regen_preview_stream(const Gen *gen)
+{
+    Gen preview_gen = {
+        .seedp = 69,
+        .step_time = gen->step_time,
+        .volume = gen->volume,
+    };
+    white_noise(&preview_gen, preview_stream, PREVIEW_STREAM_CAP);
+}
 
 int main(void)
 {
@@ -157,9 +202,12 @@ int main(void)
     }
 
     Gen gen = {
-        .step_time = 50.0f,
+        .seedp = 69,
+        .step_time = 10.0f,
         .volume = 1.0f,
     };
+
+    regen_preview_stream(&gen);
 
     SDL_AudioSpec desired = {
         .freq = FREQ,
@@ -191,12 +239,15 @@ int main(void)
         SDL_SetRenderDrawColor(renderer, HEXCOLOR(BACKGROUND_COLOR));
         SDL_RenderClear(renderer);
 
-        slider(renderer, SLIDER_FREQ,
-               100.0f, 100.0f, 500.0f,
-               &gen.step_time, 1.0f, 200.0f);
-        slider(renderer, SLIDER_VOLUME,
-               100.0f, 200.0f, 500.0f,
-               &gen.volume, 0.0f, 1.0f);
+        if (slider(renderer, SLIDER_FREQ, 100.0f, 100.0f, 500.0f, &gen.step_time, 1.0f, 50.0f)) {
+            regen_preview_stream(&gen);
+        }
+
+        if (slider(renderer, SLIDER_VOLUME, 100.0f, 200.0f, 500.0f, &gen.volume, 0.0f, 1.0f)) {
+            regen_preview_stream(&gen);
+        }
+
+        wave_preview(renderer, 100.0f, 300.0f, preview_stream, PREVIEW_STREAM_CAP);
 
         SDL_RenderPresent(renderer);
     }
